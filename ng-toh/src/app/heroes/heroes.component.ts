2c4eb4e73e9be5514b26d15a42dc00e0debbc604
rcamps07 <78983725+rcamps07@users.noreply.github.com>
2023-01-11 11:39:13 -0700

    chapter-3-complete

--- ng-toh/src/app/heroes/heroes.component.ts
+++ ng-toh/src/app/heroes/heroes.component.ts
@@ -10,11 +10,6 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent {
     heroes = HEROES;
 
-    hero : Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
-
     selectedHero?: Hero;
     onSelect(hero: Hero): void {
       this.selectedHero = hero;
